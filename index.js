197fbd23829f8fce0cd64f01583f1dcdf3ae199f
Alvaro-01 <alvaro.aguirremeza@georgebrown.ca>
2023-10-01 22:08:42 -0400

    wk4

--- index.js
+++ index.js
@@ -3,33 +3,44 @@ var express = require('express')
 const SERVER_PORT = 8089
 var app = express()
 app.use(express.json())
-app.unsubscribe(express.urlencoded({extended:true})) 
+app.unsubscribe(express.urlencoded({extended:true}))
 
-app.get("/hello", (req, res) => {
-   
-    res.send("<h1>Hello Express JS</h1>")
+app.get("/", (req, res) => {
+    res.setHeader(
+        "Content-Type", "text/play"
+    )
+    res.status(201).send("<h1>Welcome to Express Web Server</h1>")
     
-    //res.end("<h1>Welcome to Express Web Server</h1>")
+    
+    
+})
+//http://localhost:8089/hello
+app.get("/hello", (req, res) => {
+    
+    res.send("Hello Express JS")
 })
 
-app.get("/user/:fname/:lname", (req, res) => {
-   
+//path parameter
+//http://localhost:8089/user/alvaro/aguirre
+app.get("/user/:fname/:lname",(req,res)=>{
     const data = req.params
     let fnm = req.params.fname
     let lnm = req.params.lname
     res.send(data)
-    
-    
 })
 
-
-app.post("/user", (req, res) => {
-   
+//query query
+//http://localhost:8089/user?fnm=alvaro&lnm=aguirre
+app.post("/user",(req,res)=>{
     const data = req.query
-    if(data.fname == undefined && data.lname == undefined){
+    if(data.lnm == undefined && data.fnm == undefined){
         res.send("Required full name ")
     }else{
          res.send(data)
     }
-    
+   
+})
+
+app.listen(SERVER_PORT, ()=> {
+    console.log(`Server running at http://localhost:${SERVER_PORT}/`)
 })
