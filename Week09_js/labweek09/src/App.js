c61605e5b114538f46025d82167d63e544d4225c
Alvaro-01 <alvaro.aguirremeza@georgebrown.ca>
2023-11-07 22:56:02 -0500

    wk9

---
+++ Week09_js/labweek09/src/App.js
@@ -0,0 +1,23 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Welcome to Fullstack Development - 1
+        </p>
+       
+        React JS Programming Week09 Lab exercise
+        <h6>101349908</h6>
+        <h6>Alvaro Aguirre Meza</h6>
+        <h7>George Brown College</h7>
+      </header>
+      
+    </div>
+  );
+}
+
+export default App;
